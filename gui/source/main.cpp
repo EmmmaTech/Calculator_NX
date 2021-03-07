e53f74dd701c74a4c82d635c7f5807acd0df9485
EmreTech <emreterzioglu49@gmail.com>
2021-03-07 10:06:59 -0800

    Removed unneeded includes

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,9 +1,6 @@
 #include "gui_app.hpp"
 #include "app.hpp"
 
-#include <filesystem>
-#include "constants.hpp"
-
 int main(int argc, char* argv[]) {
     if (run_gui_app()) run_app();
     return EXIT_SUCCESS;
